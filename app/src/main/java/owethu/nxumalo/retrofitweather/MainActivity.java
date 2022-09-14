a5cfaf2cb50ba878efb4785f79e4fa0948acb545
lab_services_student <st10116785@vcconnect.edu.za>
2022-09-14 08:57:13 +0200

    initial commit

---
+++ app/src/main/java/owethu/nxumalo/retrofitweather/MainActivity.java
@@ -0,0 +1,14 @@
+package owethu.nxumalo.retrofitweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
